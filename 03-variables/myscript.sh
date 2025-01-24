13d52417c7aca78b35afc2dea12708e60b3adad5
AndrewChacon <andrew.b.chacon@gmail.com>
2025-01-23 19:56:19 -0500

    capturing the output of a command in a variable to use in our script

--- 03-variables/myscript.sh
+++ 03-variables/myscript.sh
@@ -6,3 +6,28 @@
 # echo "Hello, my name is $myname."
 # echo "I'm $myage years old."
 
+# word="awesome"
+
+# echo "Linux is $word"
+# echo "Video games are $word"
+# echo "Sunny days are $word"
+
+# files=$(ls) # a sub-shell
+# currentdir=$(pwd)
+# echo $currentdir
+
+# now=$(date)
+# echo "The system time and date is:"
+# echo $now
+
+# echo "The system time and date is:"
+# date
+
+name="Andrew Chacon"
+now=$(date)
+echo "Hello $name"
+echo "The system time and date is:"
+echo $now
+echo "Your username is: $USER" # default vars that exist
+
+# env shows these variables
