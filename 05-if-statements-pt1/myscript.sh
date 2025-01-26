c394dd050410c60eaaf95236f43a7bbb71c997bb
AndrewChacon <andrew.b.chacon@gmail.com>
2025-01-25 21:55:57 -0500

    changes

--- 05-if-statements-pt1/myscript.sh
+++ 05-if-statements-pt1/myscript.sh
@@ -6,9 +6,11 @@ if [ $mynum -eq 200 ] # if condtion, -eq=equals
 then
     # code block for met condition
     echo "the condition is true."
+else
+    echo "the variable does not equal 200."
 fi # end of the if statement
 
-if [ $mynum -eq 300 ]
-then
-    echo "the variable does not equal 200."
-fi
+# if [ $mynum -eq 300 ]
+# then
+#     echo "the variable does not equal 200."
+# fi
