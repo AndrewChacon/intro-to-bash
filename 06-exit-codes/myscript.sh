454c1d428f1e606277a5f4953678f6c5ea33485a
AndrewChacon <andrew.b.chacon@gmail.com>
2025-01-27 17:21:28 -0500

    more efficent way of utilizing exit codes

--- 06-exit-codes/myscript.sh
+++ 06-exit-codes/myscript.sh
@@ -4,7 +4,17 @@
 # $? contains status if previous command was successful or not
 # use exit codes to help navigate errors and take action 
 
-package=notexist
+package=ls
 
-sudo apt install $package
-echo "The exit code for the package installed is: $?"
+sudo apt install $package >> package_install_results.log
+# sudo apt remove package_name
+# echo "The exit code for the package installed is: $?"
+
+if [ $? -eq 0 ]
+then
+    echo "The install of $package was successful"
+    echo "The new command is available here:"
+    which $package
+else
+    echo "$package failed to install." >> package_install_failure.log
+fi
