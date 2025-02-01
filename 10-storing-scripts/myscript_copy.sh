3a94e875b63befd42d634d00b427c4707e2b658a
AndrewChacon <andrew.b.chacon@gmail.com>
2025-02-01 04:20:13 -0500

    script runs on kali distribution

--- 10-storing-scripts/myscript_copy.sh
+++ 10-storing-scripts/myscript_copy.sh
@@ -6,9 +6,16 @@ then
     sudo pacman -Syu
 fi
 
-if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
+if grep -q "Ubuntu" $release_file || grep -q "debian" $release_file || grep -q "kali" $release_file
 then
     sudo apt update
     sudo apt dist-upgrade
 fi
 
+
+# cp myscript.sh myscript_copy.sh
+# sudo mv myscript.sh /usr/local/bin/update
+# sudo chown root:root /usr/local/bin/update
+
+# /usr/local/bin/update - path of the update script
+# PATH - shell uses this to find a script or binary to run
